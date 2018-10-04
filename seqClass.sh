2af2448b3372a6d5f3e06e09886a0037f98c26f3
Ubuntu <uvic@ubuntu.(none)>
2018-10-04 02:30:28 -0700

    Explicacion_script

--- seqClass.sh
+++ seqClass.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-seq=$1
-seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
-if [[ $seq =~ ^[ACGTU]+$ ]]; then
-  if [[ $seq =~ T ]]; then
-    echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then
-    echo "The sequence is RNA"
-  else
-    echo "The sequence can be DNA or RNA"
+seq=$1 #Guarda los valores entrados en una variable
+seq=$(echo $seq | tr a-z A-Z)  # Pasa a mayusculas los valores entrados
+if [[ $seq =~ ^[ACGTU]+$ ]]; then #Si el valor entrado contiene ACGTU entrara dentro
+  if [[ $seq =~ T ]]; then #Si contiene una T entrara dentro
+    echo "The sequence is DNA" #Mostrara el mensaje
+  elif [[ $seq =~ U ]]; then #Si contiene una U entrara dentro
+    echo "The sequence is RNA" #Mostrara el mensaje
+  else #Si no contiene U/T entrara dentro
+    echo "The sequence can be DNA or RNA" #Mostrara el mensaje
   fi
-else
-  echo "The sequence is not DNA nor RNA"
-fi
+else #Si no contiene ninguno de los valores anteriores entrara dentro
+  echo "The sequence is not DNA nor RNA" #Mostrara el mensaje
+fi #Acaba el programa
